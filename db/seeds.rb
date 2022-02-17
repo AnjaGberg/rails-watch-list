29ccb23628a6e99e385ea6face3bbe2c80711237
Anja Greifenberg <anja.gberg@gmx.de>
2022-02-17 17:23:39 +0100

    everything but delete works so far

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Movie.create(title: "Knives Out", overview: "When renowned crime novelist Harlan Thrombey is found dead at his estate just after his 85th birthday, the inquisitive and debonair Detective Benoit Blanc is mysteriously enlisted to investigate. From Harlan's dysfunctional family to his devoted staff, Blanc sifts through a web of red herrings and self-serving lies to uncover the truth behind Harlan's untimely death.", poster_url: "https://www.themoviedb.org/t/p/w500/pThyQovXQrw2m0s9x82twj48Jq4.jpg", rating: 8.7)
+
+puts "movie created"
